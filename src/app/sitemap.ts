56b7e30ed83f2dcdf678ac10b697bcc7eee5fff0
umesh kumar bedi <umeshkumarbedi@gmail.com>
2025-07-29 15:50:26 +0000

    after submitting sitemap.xml created by site on google search console, g

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,9 @@
-import { getMovies } from '@/lib/data';
 import { MetadataRoute } from 'next';
 
 const URL = 'https://allmoviesdownload.com';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { movies } = await getMovies({ getAll: true });
-
-  const movieEntries: MetadataRoute.Sitemap = movies.map((movie) => ({
-    url: `${URL}/movies/${movie.id}`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: 0.8,
-  }));
-
+  
   return [
     {
       url: URL,
@@ -20,6 +11,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'daily',
       priority: 1,
     },
-    ...movieEntries,
   ];
 }
